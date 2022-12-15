b2b548e56e0fedfcd6dc11bbe1a4e21dcd9ff30e
sammcgeown <sammcgeown@users.noreply.github.com>
2022-12-15 10:38:18 +0000

    deploy: vmware-aria-graph-examples/vmware-aria-graph-examples@c07f709481410eccae6a02c9bcb72d0788e0bdba

--- js/clr-toggle-darkmode.js
+++ js/clr-toggle-darkmode.js
@@ -13,26 +13,21 @@ function toggleThemeMode(mode) {
 
     if(mode == "light") {
         // Enable light mode
-        console.log('enable light mode')
         darkModeCss.disabled = true;
         lightModeCss.disabled = false;
         toggleIcon.setAttribute("shape", "moon");
         localStorage.setItem("clarity-theme-mode", "light");
     } else {
-        console.log('enable dark mode')
         darkModeCss.disabled = false;
         lightModeCss.disabled = true;
         toggleIcon.setAttribute("shape", "sun");
         localStorage.setItem("clarity-theme-mode", "dark");
     }
-    console.log(localStorage.getItem('clarity-theme-mode'))
-
 }
 
 function setCurrentTheme() {
     const currentTheme = localStorage.getItem('clarity-theme-mode') ? localStorage.getItem('clarity-theme-mode') : null;
     if(currentTheme) {
-        console.log(currentTheme)
         toggleThemeMode(currentTheme)
     }
 }
